ce2e41a2fb575baa62de970d1285108fc6856e68
Akshay Dohroo <akshay.dohroo3@gmail.com>
2021-05-24 15:33:06 +0530

    Base navbar created , fine tuning it now

---
+++ src/ui-components/NavBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import { IoLogoPlaystation } from "react-icons/io";
+import styles from "./NavBar.module.css";
+function NavBar() {
+  return (
+    <Navbar bg="none" expand="lg" variant="light">
+      <Navbar.Brand href="#home" className="ml-2" >
+        <IoLogoPlaystation size={38} />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" className={styles['toggle-btn']}/>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className={styles["nav-items"]}>
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">About</Nav.Link>
+          <Nav.Link href="#link">Contact</Nav.Link>
+          <Nav.Link href="#link">Trends</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default NavBar;
