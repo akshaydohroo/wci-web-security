2d55f2d3d8ef208f8748e38a2aac8e4123761526
Akshay Dohroo <akshay.dohroo3@gmail.com>
2021-05-27 13:06:02 +0530

    dropdown for small screens added

--- src/ui-components/NavBar.js
+++ src/ui-components/NavBar.js
@@ -6,42 +6,43 @@ import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
+import ListGroup from "react-bootstrap/ListGroup";
 import { IoLogoPlaystation } from "react-icons/io";
 import { FaSearch } from "react-icons/fa";
 import Collapse from "react-bootstrap/Collapse";
 import styles from "./NavBar.module.css";
 function NavBar() {
-  const navHeight = React.useRef(null);
-  // const [hasBackgroundColor, setBackGroundColor] = React.useState(false);
- 
-
   const [collapse, setCollapse] = React.useState(true);
+  const [colorChange, setColorchange] = React.useState(false);
+  const [dropdownCollapse,setdropdownCollapse] = React.useState(false);
   const onClickCollapseHandler = () => {
     setCollapse((oldValue) => {
       return !oldValue;
     });
   };
+  const onClickDropdownCollapseHandler=()=>{
+    setdropdownCollapse((oldValue) => {
+      return !oldValue;
+    });
+  }
   const onSubmitHandler = (event) => {
     event.preventDefault();
     console.log(event);
   };
-  const [colorChange, setColorchange] = React.useState(false);
-  const changeNavbarColor = () =>{
-     if(window.scrollY >= 10){
-       setColorchange(true);
-     }
-     else{
-       setColorchange(false);
-     }
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 10) {
+      setColorchange(true);
+    } else {
+      setColorchange(false);
+    }
   };
-  window.addEventListener('scroll', changeNavbarColor);
+  window.addEventListener("scroll", changeNavbarColor);
   return (
     <Navbar
       expand="lg"
       variant="light"
       className={styles["app-nav"]}
-      ref={navHeight}
-      {...colorChange ? {bg:"light"}:{bg:"none"}}
+      {...(colorChange ? { bg: "light" } : { bg: "none" })}
     >
       <Navbar.Brand href="#home" className="ml-2">
         <IoLogoPlaystation size={38} />
@@ -59,7 +60,11 @@ function NavBar() {
           <Nav.Link href="#link">About</Nav.Link>
           <Nav.Link href="#link">Contact</Nav.Link>
           <Nav.Link href="#link">Trends</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown
+            title="Dropdown"
+            id="basic-nav-dropdown"
+            className={styles["nav-dropdown"]}
+          >
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
               Another action
@@ -70,16 +75,32 @@ function NavBar() {
               Separated link
             </NavDropdown.Item>
           </NavDropdown>
+          <div className={styles["nav-small-dropdown"]}>
+            <Button variant="outline-primary" onClick={onClickDropdownCollapseHandler}>Dropdown &#8681;</Button>
+            <Collapse in={dropdownCollapse}>
+            <ListGroup variant="flush">
+              <ListGroup.Item as="a">Cras justo odio</ListGroup.Item>
+              <ListGroup.Item as="a">Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item as="a">Morbi leo risus</ListGroup.Item>
+              <ListGroup.Item as="a">Porta ac consectetur ac</ListGroup.Item>
+              <ListGroup.Item as="a">Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+            </Collapse>
+          </div>
         </Nav>
       </Navbar.Collapse>
-    
+
       <Form inline className={styles["search-form"]} onSubmit={onSubmitHandler}>
         <InputGroup>
-          <Collapse in={collapse} dimension="width" style={{transitionDuration:"1s"}}>
+          <Collapse
+            in={collapse}
+            dimension="width"
+            style={{ transitionDuration: "1s" }}
+          >
             <div>
               <FormControl
                 id="inlineFormInputGroup"
-                placeholder="Username"
+                placeholder="Search"
                 className={styles["search-form-control"]}
               />
             </div>
