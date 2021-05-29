55ce3d615f54cbd1e87751ebf01e3a25425c34b5
TanishqAhluwalia25 <tanishqwalia25@gmail.com>
2021-05-29 13:48:12 +0530

    Added new cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import Card from "../ui-components/Cards/Card";
 import Cards2 from "../ui-components/Cards2/Cards2";
+import Cards3 from "../ui-components/Cards3/Cards3";
+import Hscroll from "../ui-components/Hscroll/Hscroll";
 import NavBar from "../ui-components/NavBar";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
-      <Cards2 />
+      {/* <Cards2 /> */}
+      {/* <Hscroll /> */}
+      <Cards3 />
     </div>
   );
 };
